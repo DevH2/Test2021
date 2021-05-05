5ee4d4f8c4b1f9770e9d9e6918a27e748c8c0043
AkemiYu <43460886+AkemiYu@users.noreply.github.com>
2021-05-04 23:02:06 -0700

    adding constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int ARM_MOTOR_CAN_ID = 4;
+    public static final int ARM_MOTOR_SWITCH_ID = 0;
+
+    public static final int moveUp = 0.7;
+    public static final int moveDown = -0.2;
+    public static final int stop = 0;
+}
